0429983c659c989a3ea5dc191a48868f3852188b
Pranav Vashi <neobuddy89@gmail.com>
2018-09-01 15:34:46 +0530

    msm8998: Switch to basic USB hal
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -406,7 +406,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service \
+    android.hardware.usb@1.0-service.basic \
     com.android.future.usb.accessory
 
 # Vendor security patch level
